6d9238a365e13544bfde3317d507cfe919abef5c
Daniel Perkins <dbp52@LAPTOP-PRU1547H>
2023-10-26 11:49:47 -0600

    Finished Markov Chains

--- MarkovChains/markov_chains.py
+++ MarkovChains/markov_chains.py
@@ -51,7 +51,8 @@ class MarkovChain:
         i = 0           # for iteration
         for column in A.T:
             if abs(sum(column) - 1) > 0.0000001:     # Verify that the matrix is column stochastic
-                print(column, sum(column))
+                print(i, column)
+                print(sum(column))
                 raise ValueError("The Matrix is not column stochastic")
             self.label_dict[self.labels[i]] = i    # Set each label to map to column number
             i += 1
@@ -125,7 +126,17 @@ class MarkovChain:
         Raises:
             ValueError: if there is no convergence within maxiter iterations.
         """
-        raise NotImplementedError("Problem 4 Incomplete")
+        x = np.random.random(len(self.A))     # random vector
+        x = x / np.linalg.norm(x, ord=1)      # Normalize so that it adds to one
+        x_old = np.zeros_like(x)    # to allow for while loop
+        k = 0                 # find number of iters
+        while la.norm(x_old - x, ord=1) > tol:
+            if k > maxiter:
+                raise ValueError("A^k does not converge")
+            x_old = x
+            x = self.A @ x    # Multiplty x_k+1=Ax_k
+            k += 1    # Keep track of iterations
+        return x
 
 class SentenceGenerator(MarkovChain):
     """A Markov-based simulator for natural language.
@@ -139,7 +150,45 @@ class SentenceGenerator(MarkovChain):
         contents. You may assume that the file has one complete sentence
         written on each line.
         """
-        raise NotImplementedError("Problem 5 Incomplete")
+        # Read the file
+        with open(filename, "r") as file:
+            data = file.readlines()
+        data = [line.split() for line in data]
+
+        # Get the state labels
+        set_of_words = set()   # Use to store each word without repitition
+        for sentence in data:
+            for word in sentence:
+                set_of_words.add(word)
+        words = list(set_of_words)   # Make it a set to preserve order
+        words.insert(0, "$tart")
+        words.append("$top")
+
+        # Create Matrix of zeroes of appropriate size
+        A = np.zeros((len(words),len(words)))
+        # self.A = MarkovChain(A, set_of_words)
+
+        # Create training set
+        for sentence in range(len(data)):
+            # Prepend start and end
+            data[sentence].insert(0, "$tart")
+            data[sentence].append("$top")
+            prev_word = data[sentence][0] # Start with first word
+            for word in data[sentence][1:]:  # For each word (exludint the first since nothing was before it)
+                A[words.index(word), words.index(prev_word), ] += 1   # Add 1 to entry from previous word to current word
+                prev_word = word        # Update previous word
+        A[len(A) - 1, len(A) - 1] = 1   # Map End to itself
+
+        # Normalize the columns
+        norm = np.linalg.norm(A, ord=1, axis=0)
+        A = A / norm
+
+        # Set all attributes
+        chain = MarkovChain(A, words)
+        self.A = chain.A  # Save training set as Markov Chain
+        self.labels = chain.labels
+        self.label_dict = chain.label_dict
+
 
     # Problem 6
     def babble(self):
@@ -154,20 +203,43 @@ class SentenceGenerator(MarkovChain):
             >>> print(yoda.babble())
             The dark side of loss is a path as one with you.
         """
-        raise NotImplementedError("Problem 6 Incomplete")
+        sentence = self.path("$tart", "$top")
+        sentence = " ".join(sentence[1:-1])
+        return sentence
 
 if __name__=="__main__":
-    A = np.array([[0.5, 0.3, 0.1, 0], [0.3, 0.3, 0.3, 0.3], [0.2, 0.3, 0.4, 0.5], [0, 0.1, 0.2, 0.2]])
-    my_chain = MarkovChain(A, ["hot", "mild", "cold", "freezing"])
+    # A = np.array([[0.5, 0.3, 0.1, 0], [0.3, 0.3, 0.3, 0.3], [0.2, 0.3, 0.4, 0.5], [0, 0.1, 0.2, 0.2]])
+    # # A = np.array([[.7, .6], [.3, .4]])
+    # my_chain = MarkovChain(A, ["hot", "mild", "cold", "freezing"])
 
-    # Prob 1
-    # print(my_chain.A)
-    # print(my_chain.labels)
-    # print(my_chain.label_dict)
+    # # Prob 1
+    # # print(my_chain.A)
+    # # print(my_chain.labels)
+    # # print(my_chain.label_dict)
 
-    # Prob 2
-    # print(my_chain.transition("cold"))
+    # # Prob 2
+    # # print(my_chain.transition("cold"))
 
-    # Prob 3
-    # print(my_chain.walk("mild", 5))
-    # print(my_chain.path("hot", "freezing"))
+    # # Prob 3
+    # # print(my_chain.walk("mild", 5))
+    # # print(my_chain.path("hot", "freezing"))
+
+    # # Prob 4 Testing
+    # # x = my_chain.steady_state()
+    # # print(my_chain.A @ x)
+    # # print(np.linalg.matrix_power(my_chain.A, 15))
+
+    # # Prob 4, validate results for prob 3
+    # B = np.array([[.7, .6], [.3, .4]])
+    # my_chain = MarkovChain(B, ["hot", "cold"])
+    # steady_state = my_chain.steady_state()
+    # print("steady state", steady_state)
+    # large_walk = my_chain.walk("hot", 100)
+    # print("num of hot days in long walk of 100: ", sum(np.core.defchararray.count(large_walk, "hot")))
+
+    # Prob 5 and 6
+    # chain = SentenceGenerator("yoda.txt")
+    # for _ in range(5):
+    #     print(chain.babble())
+    #     print()
+    print()
